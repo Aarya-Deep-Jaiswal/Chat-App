7fec5aa98fe9dc4d5e136f3255c9401f3f21ad9b
Aarya-Deep-Jaiswal <aaryadeep.official@gmail.com>
2024-12-08 13:11:30 +0530

    final

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import messageRoutes from "./routes/messageRoute.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 //socket io part
 
@@ -13,6 +14,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
@@ -26,6 +28,14 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
