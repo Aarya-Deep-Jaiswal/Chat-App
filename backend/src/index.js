614743187fad20e193230b8ece426e23b2b1d1e6
Aarya-Deep-Jaiswal <aaryadeep.official@gmail.com>
2024-12-08 12:40:03 +0530

    project-completed

---
+++ backend/src/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoute.js";
+import messageRoutes from "./routes/messageRoute.js";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+//socket io part
+
+import { app, server } from "./lib/socket.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  connectDB();
+});
